a7801190912d69fa55c7a1b0cfae774261b48ba9
hibiki <hibiki123123@gmail.com>
2025-01-26 19:32:22 +0900

    Refactor next.config.js to enable logging only in development and test environments

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -11,6 +11,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { db } from "~/server/db";
+import { getLogger } from "~/lib/logger/server";
 
 /**
  * 1. CONTEXT
@@ -81,14 +82,18 @@ export const createCallerFactory = t.createCallerFactory;
  */
 export const createTRPCRouter = t.router;
 
+const logger = getLogger("trpc");
+
 /**
  * Middleware for timing procedure execution and adding an artificial delay in development.
  *
  * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
  * network latency that would occur in production but not in local development.
  */
-const timingMiddleware = t.middleware(async ({ next, path }) => {
+const timingMiddleware = t.middleware(async (opts) => {
   const start = Date.now();
+
+  logger.info("[Request] " + opts.path);
 
   if (t._config.isDev) {
     // artificial delay in dev
@@ -96,10 +101,22 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
     await new Promise((resolve) => setTimeout(resolve, waitMs));
   }
 
-  const result = await next();
+  const result = await opts.next();
 
   const end = Date.now();
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+  const duration = end - start;
+  // duration to sec
+  const durationSec = duration / 1000;
+
+  logger.info(
+    "[Response] " +
+      opts.path +
+      ", execTime " +
+      durationSec +
+      "s" +
+      ", status " +
+      (result.ok ? "success" : "error"),
+  );
 
   return result;
 });
